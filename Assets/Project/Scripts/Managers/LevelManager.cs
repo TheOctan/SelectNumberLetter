bc83de4a2cbdc57f0c8415af3c501992ab654cbe
TheOctan <kanatush.tim@gmail.com>
2021-07-07 12:11:27 +0300

    task generator

--- Assets/Project/Scripts/Managers/LevelManager.cs
+++ Assets/Project/Scripts/Managers/LevelManager.cs
@@ -5,71 +5,83 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class LevelManager : MonoBehaviour
+namespace OctanGames
 {
-	[Range(1, 3)]
-	[SerializeField] int _countLevels = 3;
-	[SerializeField] private float _startDelay = 0.1f;
-
-	[Header("Components")]
-	[SerializeField] private TaskGenerator _taskGenerator;
-
-	[Header("Panel settings")]
-	[SerializeField] private Panel _panel;
-	[SerializeField] private float _inDuration = 1f;
-	[SerializeField] private Ease _panelEaseType = Ease.Linear;
-
-	[Header("Task settings")]
-	[SerializeField] private TextMeshProUGUI _taskLabel;
-	[SerializeField] private float _fadeDuration = 1f;
-	[SerializeField] private Ease _taskEaseType = Ease.Linear;
-
-	[Header("Events")]
-	[SerializeField] private LevelEvent _levelStarted;
-	[SerializeField] private UnityEvent _gameFinished;
-
-	private Color _taskLabelColor;
-	private int _currentLevel = 1;
-
-
-	private void Awake()
+	public class LevelManager : MonoBehaviour
 	{
-		InitMenu();
-	}
+		[Range(1, 3)]
+		[SerializeField] int _countLevels = 3;
+		[SerializeField] private float _startDelay = 0.1f;
 
-	private void Start()
-	{
-		StartGame();
-	}
+		[Header("Components")]
+		[SerializeField] private TaskGenerator _taskGenerator;
 
-	private void Update()
-	{
+		[Header("Panel settings")]
+		[SerializeField] private Panel _panel;
+		[SerializeField] private float _inDuration = 1f;
+		[SerializeField] private Ease _panelEaseType = Ease.Linear;
+
+		[Header("Task settings")]
+		[SerializeField] private TextMeshProUGUI _taskLabel;
+		[SerializeField] private float _fadeDuration = 1f;
+		[SerializeField] private Ease _taskEaseType = Ease.Linear;
+
+		[Header("Events")]
+		[SerializeField] private LevelEvent _levelStarted;
+		[SerializeField] private UnityEvent _gameFinished;
+
+		private Color _taskLabelColor;
+		private int _currentLevel = 1;
+
+
+		private void Awake()
+		{
+			InitMenu();
+		}
+
+		private void Start()
+		{
+			StartGame();
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Space))
+			{
+				UpdateTask();
+			}
+		}
+
+		private void StartGame()
+		{
+			_levelStarted.Invoke(_currentLevel);
+			UpdateTask();
+			AnimateMenu();
+		}
 		
+		private void UpdateTask()
+		{
+			_taskLabel.text = _taskGenerator.GetNextTask();
+		}
+
+		private void InitMenu()
+		{
+			_taskLabelColor = _taskLabel.color;
+			_taskLabel.color = new Color(0, 0, 0, 0);
+			_panel.transform.localScale = new Vector3(0, 0, 0);
+		}
+
+		private void AnimateMenu()
+		{
+			DOTween.Sequence()
+				.AppendInterval(_startDelay)
+				.Append(_panel.transform.DOScale(1, _inDuration).SetEase(_panelEaseType))
+				.Append(_taskLabel.DOColor(_taskLabelColor, _fadeDuration).SetEase(_taskEaseType));
+		}
 	}
 
-	private void StartGame()
+	[System.Serializable]
+	public class LevelEvent : UnityEvent<int>
 	{
-		_levelStarted.Invoke(_currentLevel);
-		AnimateMenu();
 	}
-
-	private void InitMenu()
-	{
-		_taskLabelColor = _taskLabel.color;
-		_taskLabel.color = new Color(0, 0, 0, 0);
-		_panel.transform.localScale = new Vector3(0, 0, 0);
-	}
-
-	private void AnimateMenu()
-	{
-		DOTween.Sequence()
-			.AppendInterval(_startDelay)
-			.Append(_panel.transform.DOScale(1, _inDuration).SetEase(_panelEaseType))
-			.Append(_taskLabel.DOColor(_taskLabelColor, _fadeDuration).SetEase(_taskEaseType));
-	}
-}
-
-[System.Serializable]
-public class LevelEvent : UnityEvent<int>
-{
 }
--- Assets/Project/Scripts/TaskGenerator.cs
+++ Assets/Project/Scripts/TaskGenerator.cs
@@ -2,43 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TaskGenerator : MonoBehaviour
+namespace OctanGames
 {
-	[Header("Task info")]
-	[SerializeField] private string _taskTemplate;
-	
-	public List<Dictionary> Sets = new List<Dictionary>();
-	//public Dictionary ActiveSet => Sets.Count > 0 ? Sets[_activeSetIndex] : null;
-
-
-
-
-	private int _activeSetIndex = 0;
-
-	private void Start()
+	public class TaskGenerator : MonoBehaviour
 	{
-		
-    }
+		[Header("Task info")]
+		[SerializeField] private string _taskTemplate;
+		[SerializeField] private bool _randomizeWithRestart;
 
-	public void OnLevelStarted(int level)
-	{
+		public List<ItemSet> Sets = new List<ItemSet>();
+		public ItemSet ActiveSet => Sets.Count > 0 ? Sets[_activeSetIndex] : null;
 
-	}
+		private System.Random _random = new System.Random(System.DateTime.Now.Second);
+		private int _activeSetIndex = 0;
 
-	public void OnGameFinished()
-	{
+		private void Awake()
+		{
+			RandomizeSets();
+		}
 
-	}
+		public void OnGameFinished()
+		{
+			if (_randomizeWithRestart)
+			{
+				RandomizeSets();
+			}
+		}
 
-	public string GetNextTask()
-	{
-		throw new System.NotImplementedException();
+		public string GetNextTask()
+		{
+			if (Sets.Count > 1)
+			{
+				_activeSetIndex = _random.Next(0, Sets.Count);
+			}
 
-	}
+			var item = Sets[_activeSetIndex].GetNextRandomItem();
+			return string.Format(_taskTemplate, item.Name);
+		}
 
-	private void GenerateSet()
-	{
-
+		private void RandomizeSets()
+		{
+			for (int i = 0; i < Sets.Count; i++)
+			{
+				Sets[i].RandomizeSet();
+			}
+		}
 	}
 }
-
